64644c42b5e963ee18a1f80adfd944dbe7001a54
Filip Kaszuba <fkaszuba93@gmail.com>
2023-05-04 19:43:04 +0200

    create DevExpress document generator

--- PdfCreator/Program.cs
+++ PdfCreator/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<DocumentsService>();
+builder.Services.AddScoped<IDocumentGenerator, DevExpressDocumentGenerator>();
 
 var app = builder.Build();
 
---
+++ PdfCreator/Service/DevExpressDocumentGenerator.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.XtraRichEdit;
+using DevExpress.XtraRichEdit.API.Native;
+
+namespace PdfCreator.Service
+{
+    public class DevExpressDocumentGenerator : IDocumentGenerator
+    {
+        private readonly RichEditDocumentServer wordProcessor;
+        private readonly Document document;
+
+        public DevExpressDocumentGenerator()
+        {
+            wordProcessor = new RichEditDocumentServer();
+            document = wordProcessor.Document;
+        }
+
+        public void CreateDocument(List<string> documentData)
+        {
+            foreach (string line in documentData)
+            {
+                document.AppendText(line + "\n\n");
+            }
+
+            wordProcessor.SaveDocument("c:\\temp\\test.docx", DocumentFormat.OpenXml);
+        }
+    }
+}
