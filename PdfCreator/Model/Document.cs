4398d3b5c1b092b1065f580e015dac24b7260c1c
Filip Kaszuba <fkaszuba93@gmail.com>
2023-05-24 19:37:39 +0200

    fix issue with path property

--- PdfCreator/Model/Document.cs
+++ PdfCreator/Model/Document.cs
@@ -3,13 +3,14 @@
     public class Document
     {
         public string Id { get; set; }
-        public string? Path { get; set; }
+        public string Path { get; set; }
         public DateTime CreateDate { get; set; }
 
         public Document()
         {
             Id = Guid.NewGuid().ToString();
             CreateDate = DateTime.Now;
+            Path = "";
         }
     }
 }
