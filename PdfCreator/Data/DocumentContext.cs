58b73def708b58280f47eae34e171a1212aee51a
Filip Kaszuba <fkaszuba93@gmail.com>
2023-05-24 18:29:04 +0200

    add database connection

---
+++ PdfCreator/Data/DocumentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PdfCreator.Model;
+
+namespace PdfCreator.Data
+{
+    public class DocumentContext : DbContext
+    {
+        public DbSet<Document> Documents { get; set; }
+
+        public DocumentContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- PdfCreator/Program.cs
+++ PdfCreator/Program.cs
@@ -1,4 +1,6 @@
+using PdfCreator.Data;
 using PdfCreator.Service;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddScoped<DocumentsService>();
 builder.Services.AddScoped<IDocumentGenerator, SelectPdfDocumentGenerator>();
 
+builder.Services.AddDbContext<DocumentContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DocumentContext")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +21,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DocumentContext>();
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
